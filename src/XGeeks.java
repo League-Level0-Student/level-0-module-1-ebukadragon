14c7de6c98343d2c3b9c67536588d4b732198415
Chris Xaysanasith <chris.xaysanasith@gompersstudent.org>
2017-11-27 14:27:44 -0800

    xgeeks

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,11 +15,29 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+		String ebuka0 = "Teleportation";
+		String ebuka1 = "super speed 80000000mps";
+		String ebuka2 = "super strength 60tons";
+		String ebuka3 = "extreme healing";
+		String ebuka4 = "extreme laser projection";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+		String word=	JOptionPane.showInputDialog("what name would you choose");
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+		if(word.equals("ebuka0")){
+			JOptionPane.showMessageDialog(null, ebuka0);
+		}
+		if(word.equals("ebuka1")){
+			JOptionPane.showMessageDialog(null, ebuka1);
+		}2
+		if(word.equals("ebuka2")){
+			JOptionPane.showMessageDialog(null, ebuka2);
+		}
+		if(word.equals("ebuka3")){
+			JOptionPane.showMessageDialog(null, ebuka3);
+		}
+		if(word.equals("ebuka4")){
+			JOptionPane.showMessageDialog(null, ebuka4);
+		}
 	}
 }
 
